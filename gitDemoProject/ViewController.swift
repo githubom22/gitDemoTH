1fb2aedef8fa3c6377e339a693bf340677632aa0
Test 4 <test4@Olyas-MacBook-Pro.local>
2017-01-08 16:38:27 -0500

    Initial Commit

---
+++ gitDemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoProject
+//
+//  Created by Test 4 on 1/8/17.
+//  Copyright © 2017 Test 4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
